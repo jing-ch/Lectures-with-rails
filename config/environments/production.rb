7e4a34c31eaf45d4baa5ef08b9cd79f3c338b54c
Jing <chenjinghanlaura@gmail.com>
2022-12-18 23:52:44 +0100

    Modified the sign in sign out links, according to whether user is signed in or not.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -90,4 +90,9 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # Will edit it again when there is new server.
+  # config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
+
 end
